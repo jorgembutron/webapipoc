9de8be96ccd638bdc00e62c67759e55a562e00c8
Jorge Butron <jorgembutron@hotmail.com>
2021-05-24 19:02:42 -0500

    Fix folders

---
+++ tests/Company.Biz.Factories/VmFactory.cs
@@ -0,0 +1,12 @@
+﻿using Company.Biz.WebApi.Application.Commands;
+
+namespace Company.Biz.Factories
+{
+    public static class VmFactory
+    {
+        public static CreatePingCommand CreatePingCommandViewModel() => new CreatePingCommand
+        {
+            Name = "Integration test"
+        };
+    }
+}
